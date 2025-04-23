0abf1b0989adcf3ff134c8dd0e066212bbec8aed
yoshikouki <yoshikouki@pepabo.com>
2025-04-23 23:31:37 +0900

    Fix database URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
+import { DATABASE_URL } from "./src/db";
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: DATABASE_URL,
   },
 });
